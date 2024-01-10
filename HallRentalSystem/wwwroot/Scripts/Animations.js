416f6fa4b14161c0aacb468c2568a9371145593a
Teodor Mihail <teodormihail07@gmail.com>
2024-01-10 01:19:35 +0000

    * COMPLETED HOME PAGE STRUCTURE

--- HallRentalSystem/wwwroot/Scripts/Animations.js
+++ HallRentalSystem/wwwroot/Scripts/Animations.js
@@ -1,6 +1,7 @@
 ﻿let expansion_animation_interval = undefined;
 let gradient_fluctuation_interval = undefined;
 let background_image_sizing = undefined;
+let home_page_elements_resizing = undefined;
 let current_width = 0;
 let current_gradient = 0;
 let switch_gradient_offset = false;
@@ -10,7 +11,7 @@ let switch_gradient_offset = false;
 // [ BEGIN ]
 async function GradientFluctuationAnimationImplementation(element_id, initial_gradient, final_gradient) {
     let element = document.getElementById(element_id);
-    element.style.background = "linear-gradient(to right, rgb(22, 22, 22), " + current_gradient + "%, #999999 75%)";
+    element.style.background = "linear-gradient(to right, rgb(22, 22, 22), " + current_gradient + "%, #7D7D7D 75%)";
 
     switch (switch_gradient_offset) {
         case true:
@@ -52,6 +53,7 @@ async function ExpansionAnimationImplementation(element_id, max_width, size_unit
         element.style.width = current_width + size_unit;
     }
     else {
+        
         await clearInterval(expansion_animation_interval);
     }
     current_width++;
@@ -83,6 +85,42 @@ export async function Clear_Background_Image_Sizing() {
 
 export async function Set_Background_Image_Sizing() {
     background_image_sizing = await setInterval(async () => { await Background_Image_Sizing(); }, 1);
+}
+
+// [ END ]
+
+
+
+// HOME PAGE ELEMENT RESIZING
+//
+// [ START ]
+
+function Resize_Home_Page_Elements() {
+    let content_block_1 = document.getElementById("content_block_1");
+    let content_block_2 = document.getElementById("content_block_2");
+    let index_page_jumbotron = document.getElementById("index_page_jumbotron");
+    let half_wifth = index_page_jumbotron.offsetWidth / 2.1;
+
+    if (index_page_jumbotron.offsetWidth <= 1600) {
+
+        content_block_1.style.width = "100%";
+        content_block_2.style.width = "100%";
+        console.log("Vertical display");
+    }
+    else {
+        content_block_1.style.width = "49.8%";
+        content_block_2.style.width = "49.8%";
+    }
+}
+
+export async function Clear_Resize_Home_Page_Elements() {
+    if (home_page_elements_resizing !== undefined) {
+        await clearInterval();
+    }
+}
+
+export async function Set_Resize_Home_Page_Elements() {
+    home_page_elements_resizing = await setInterval(async () => { await Resize_Home_Page_Elements(); }, 1);
 }
 
 // [ END ]
