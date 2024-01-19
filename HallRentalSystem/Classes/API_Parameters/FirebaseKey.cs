b558bdcd2407b87bab8bd79e7483fdc0d09a7103
Teodor Mihail <teodormihail07@gmail.com>
2024-01-19 15:36:55 +0000

    * FINISHED LOGOUT * FINISHED LOG IN SESSION KEY GENERATION

---
+++ HallRentalSystem/Classes/API_Parameters/FirebaseKey.cs
@@ -0,0 +1,8 @@
+﻿namespace HallRentalSystem.Classes.API_Parameters
+{
+    public class FirebaseKey
+    {
+        public string? log_in_session_key { get; set; }
+        public string? database_key { get; set; }
+    }
+}
