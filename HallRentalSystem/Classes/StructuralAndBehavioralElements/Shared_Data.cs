6121da5e11ac20f0e4953629017af84c0793ba34
Teodor Mihail <teodormihail07@gmail.com>
2024-01-23 22:20:45 +0000

    * FEATURE UPDATE

--- HallRentalSystem/Classes/StructuralAndBehavioralElements/Shared_Data.cs
+++ HallRentalSystem/Classes/StructuralAndBehavioralElements/Shared_Data.cs
@@ -4,6 +4,7 @@ using HallRentalSystem.Classes.Models;
 using HallRentalSystem.Classes.StructuralAndBehavioralElements.Authentication;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.JSInterop;
 
 namespace HallRentalSystem.Classes.StructuralAndBehavioralElements
 {
@@ -15,7 +16,6 @@ namespace HallRentalSystem.Classes.StructuralAndBehavioralElements
             Logout
         }
         public static AuthState authentication_state = AuthState.Login;
-        public static CRUD_Context<AuthStateManagerParameters, ProtectedLocalStorage, AuthStateManagerParameters, ProtectedLocalStorage> auth_manager = new CRUD_Context<AuthStateManagerParameters, ProtectedLocalStorage, AuthStateManagerParameters, ProtectedLocalStorage>(new AuthenticationStateManager());
         public static CRUD_Context<Customer_ID_Value, Customer_ID_Value, Customers, string> authentication = new CRUD_Context<Customer_ID_Value, Customer_ID_Value, Customers, string>(new CredentialsOperations());
         public static CRUD_Context<Log_In_Session_ID_Value, string, string, FirebaseKey> log_in_session = new CRUD_Context<Log_In_Session_ID_Value, string, string, FirebaseKey>(new LoginSessionKeyOperations());
     
