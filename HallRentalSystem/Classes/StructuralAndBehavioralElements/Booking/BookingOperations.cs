09509d4a2b87e151e2dbbea24b25a9570a8a6148
Teodor Mihail <teodormihail07@gmail.com>
2024-04-20 01:47:36 +0100

    * FINISHED PAYMENT FRONTEND * INITIATED BOOKING BACKEND

--- HallRentalSystem/Classes/StructuralAndBehavioralElements/Booking/BookingOperations.cs
+++ HallRentalSystem/Classes/StructuralAndBehavioralElements/Booking/BookingOperations.cs
@@ -1,4 +1,5 @@
-﻿using HallRentalSystem.Classes.API_Parameters;
+﻿using HallRentalSystem.Classes.StructuralAndBehavioralElements;
+using HallRentalSystem.Classes.API_Parameters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HallRentalSystem.Classes.StructuralAndBehavioralElements.Booking
@@ -18,9 +19,58 @@ namespace HallRentalSystem.Classes.StructuralAndBehavioralElements.Booking
 
         public async Task<ReturnType?> Insert<ReturnType>(Booking_Parameters? data)
         {
+            if (data?.Hall_ID != null)
+            {
+                if (data?.stripe_payment_method != null)
+                {
+                    if (data?.key != null)
+                    {
+                        if (data?.rental_dates?.Count > 0)
+                        {
+                            string? log_in_session_key_verification_result = await Shared_Data.log_in_session.Get<string>(data.key);
+                            
+                            if (log_in_session_key_verification_result == "Valid login session key")
+                            {
+                                ///////////////////
+                                // !!! TO DO !!! //
+                                ///////////////////
+                                
 
-            return (ReturnType)(object)"Ok";
-            //throw new NotImplementedException();
+                                Bookings booking = new Bookings();
+
+                                return (ReturnType)(object)"Internal server error";
+                            }
+                            else
+                            {
+                                if (log_in_session_key_verification_result != null)
+                                {
+                                    return (ReturnType)(object)log_in_session_key_verification_result;
+                                }
+                                else
+                                {
+                                    return (ReturnType)(object)"Internal server error";
+                                }
+                            }
+                        }
+                        else
+                        {
+                            return (ReturnType)(object)"Missing required data";
+                        }
+                    }
+                    else
+                    {
+                        return (ReturnType)(object)"Missing required data";
+                    }
+                }
+                else
+                {
+                    return (ReturnType)(object)"Missing required data";
+                }
+            }
+            else
+            {
+                return (ReturnType)(object)"Missing required data";
+            }
         }
 
         public Task<ReturnType?> Update<ReturnType>(Bookings? data)
